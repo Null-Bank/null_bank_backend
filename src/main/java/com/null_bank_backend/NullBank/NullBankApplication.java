e333c7172bbbd48671fc9d624ca1d67774b4c42c
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-04-02 21:13:28 -0300

    Feature/init project (#3)
    * feat: Create .gitignore
    
    * feat: Init project
    
    * feat: Update README.md

---
+++ src/main/java/com/null_bank_backend/NullBank/NullBankApplication.java
@@ -0,0 +1,13 @@
+package com.null_bank_backend.NullBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NullBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NullBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/null_bank_backend/NullBank/NullBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.null_bank_backend.NullBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
