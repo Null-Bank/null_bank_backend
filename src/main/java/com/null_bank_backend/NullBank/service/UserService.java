e37b83cc6b67030f0d7d7be6b2ef9ad87d4c9ec4
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-04-07 11:07:27 -0300

    feat: Create register method

--- src/main/java/com/null_bank_backend/NullBank/service/UserService.java
+++ src/main/java/com/null_bank_backend/NullBank/service/UserService.java
@@ -1,7 +1,17 @@
 package com.null_bank_backend.NullBank.service;
 
+import com.null_bank_backend.NullBank.model.User;
+import com.null_bank_backend.NullBank.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User register(User user) {
+        return userRepository.save(user);
+    }
 }
